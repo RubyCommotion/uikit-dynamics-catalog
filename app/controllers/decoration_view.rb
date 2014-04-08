b7066aae1e5495e0830aad361d1c3892ce0b6af9
Dennis Major <dennismajor1@gmail.com>
2014-04-08 11:02:22 -0400

    Refactored DecorationView to allow additional unit tests.

--- app/controllers/decoration_view.rb
+++ app/controllers/decoration_view.rb
@@ -6,7 +6,7 @@ class DecorationView < UIView
     super
     if self
       create_arrow_view
-      @attachment_decoration_layers = create_attachment_decoration_layers
+      self.attachment_decoration_layers = create_attachment_decoration_layers
     else
       return nil
     end
@@ -57,68 +57,36 @@ class DecorationView < UIView
 
   def layoutSubviews
     super
+
     arrow_view.center = CGPointMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds))
 
-    if @center_point_view
+    if center_point_view
       center_point_view.center = CGPointMake(CGRectGetMidX(self.bounds), CGRectGetMidY(self.bounds))
     end
 
     if attachment_point_view && attached_view
-      max_dashes = attachment_decoration_layers.count
-      attachment_point_view_center = CGPointMake(self.attachment_point_view.bounds.size.width/2, self.attachment_point_view.bounds.size.height/2)
-      attachment_point_view_center = self.attachment_point_view.convertPoint(attachment_point_view_center, toView: self)
-      attached_view_attachment_point = CGPointMake(self.attached_view.bounds.size.width/2 + self.attachment_offset.x, self.attached_view.bounds.size.height/2 + self.attachment_offset.y)
-      attached_view_attachment_point =  self.attached_view.convertPoint(attached_view_attachment_point, toView: self)
-      distance = Math.sqrt( ((attached_view_attachment_point.x-attachment_point_view_center.x) ** 2.0) +
-                            ((attached_view_attachment_point.y-attachment_point_view_center.y) ** 2.0) )
-      angle = Math.atan2( attached_view_attachment_point.y-attachment_point_view_center.y,
-                          attached_view_attachment_point.x-attachment_point_view_center.x )
-      required_dashes = 0
-      d = 0.0
+
+      attachment_points = create_attachment_points
+      distance_and_angle = calc_distance_and_angle(attachment_points[:attachment_point_view_center], attachment_points[:attached_view_attachment_point])
 
       # Depending on the distance between the two views, a smaller number of dashes may be needed to adequately visualize the attachment.  Starting
       # with a distance of 0, we add the length of each dash until we exceed 'distance' computed previously or we use the maximum number of allowed
       # dashes, 'max_dashes'.
-      while required_dashes < max_dashes
-        dash_layer = attachment_decoration_layers[required_dashes]
-        if (d + dash_layer.bounds.size.height) < distance
-          d += dash_layer.bounds.size.height
-          dash_layer.hidden = false
-          required_dashes = required_dashes + 1
-        else
-          break
-        end
 
-        # Based on the total length of the dashes we previously determined were  necessary to visualize the attachment, determine the spacing between
-        # each dash.
-        dash_spacing = (distance - d) / (required_dashes + 1)
-      end
+      dashes = number_of_required_dashes(distance_and_angle[:distance])
+
+      dash_spacing = (distance_and_angle[:distance] - dashes[:d]) / (dashes[:required_dashes] + 1)
 
       # Hide the excess dashes.
-      while required_dashes < max_dashes
-        attachment_decoration_layers[required_dashes].setHidden(true)
-        required_dashes = required_dashes + 1
-      end
+      dashes[:required_dashes] = hide_dashes(dashes[:required_dashes], dashes[:max_dashes])
 
-      # Disable any animations.  The changes must take full effect immediately.
-      CATransaction.begin
-      CATransaction.setAnimationDuration(0)
 
-      # Each dash layer is positioned by altering its affineTransform.  We combine the position of rotation into an affine transformation matrix
-      # that is assigned to each dash.
-      transform = CGAffineTransformMakeTranslation(attachment_point_view_center.x, attachment_point_view_center.y)
-      transform = CGAffineTransformRotate(transform, angle - Math::PI/2)
+      transform_object = make_transform_object(attachment_points[:attachment_point_view_center], distance_and_angle[:angle])
+      do_transform(dashes[:required_dashes], dashes[:dash_layer], dash_spacing, transform_object)
 
-      drawn_dashes = 0
-      while (drawn_dashes < required_dashes)
-        dash_layer = attachment_decoration_layers[drawn_dashes]
-        transform = CGAffineTransformTranslate(transform, 0, dash_spacing)
-        dash_layer.setAffineTransform(transform)
-        transform = CGAffineTransformTranslate(transform, 0, dash_layer.bounds.size.height)
-        drawn_dashes = drawn_dashes + 1
-      end
-      CATransaction.commit
     end
+
+
   end
 
 
@@ -132,6 +100,79 @@ class DecorationView < UIView
   end
 
   private
+
+  def calc_distance_and_angle(attachment_point_view_center, attached_view_attachment_point)
+
+    distance_and_angle = {distance:nil, angle: nil}
+
+    distance_and_angle[:distance] = Math.sqrt( ((attached_view_attachment_point.x-attachment_point_view_center.x) ** 2.0) +
+                          ((attached_view_attachment_point.y-attachment_point_view_center.y) ** 2.0) )
+    distance_and_angle[:angle] = Math.atan2( attached_view_attachment_point.y-attachment_point_view_center.y,
+                        attached_view_attachment_point.x-attachment_point_view_center.x )
+    distance_and_angle
+  end
+
+  def create_attachment_points
+
+    attachment_points = {attachment_point_view_center:nil, attached_view_attachment_point: nil}
+
+    attachment_points[:attachment_point_view_center] = CGPointMake(self.attachment_point_view.bounds.size.width/2, self.attachment_point_view.bounds.size.height/2)
+    attachment_points[:attachment_point_view_center] = self.attachment_point_view.convertPoint(attachment_points[:attachment_point_view_center], toView: self)
+    attachment_points[:attached_view_attachment_point] = CGPointMake(self.attached_view.bounds.size.width/2 + self.attachment_offset.x, self.attached_view.bounds.size.height/2 + self.attachment_offset.y)
+    attachment_points[:attached_view_attachment_point] =  self.attached_view.convertPoint(attachment_points[:attached_view_attachment_point], toView: self)
+    attachment_points
+  end
+
+
+  def number_of_required_dashes(distance)
+    dashes = {required_dashes: 0, d: 0.0, max_dashes:attachment_decoration_layers.count, dash_layer: nil }
+
+    dashes[:d] = 0.0
+    dashes[:max_dashes] = attachment_decoration_layers.count
+
+    while dashes[:required_dashes] < dashes[:max_dashes]
+      dashes[:dash_layer] = attachment_decoration_layers[dashes[:required_dashes]]
+      if (dashes[:d] + dashes[:dash_layer].bounds.size.height) < distance
+        dashes[:d] += dashes[:dash_layer].bounds.size.height
+        dashes[:dash_layer].hidden = false
+        dashes[:required_dashes] = dashes[:required_dashes] + 1
+      else
+        break
+      end
+    end
+    dashes
+  end
+
+  def hide_dashes(required_dashes, max_dashes)
+    while required_dashes < max_dashes
+      attachment_decoration_layers[required_dashes].setHidden(true)
+      required_dashes = required_dashes + 1
+    end
+    required_dashes
+  end
+
+  def make_transform_object(attachment_point_view_center, angle)
+    # Disable any animations. The changes must take full effect immediately.
+    CATransaction.begin
+    CATransaction.setAnimationDuration(0)
+      # Each dash layer is positioned by altering its affineTransform.  We combine the position of rotation into an affine transformation matrix
+      # that is assigned to each dash.
+      transform = CGAffineTransformMakeTranslation(attachment_point_view_center.x, attachment_point_view_center.y)
+      transform = CGAffineTransformRotate(transform, angle - Math::PI/2)
+  end
+
+  def do_transform(required_dashes, dash_layer, dash_spacing, transform_object)
+    drawn_dashes = 0
+    while drawn_dashes < required_dashes
+      dash_layer = attachment_decoration_layers[drawn_dashes]
+      transform_object = CGAffineTransformTranslate(transform_object, 0, dash_spacing)
+      dash_layer.setAffineTransform(transform_object)
+      transform_object = CGAffineTransformTranslate(transform_object, 0, dash_layer.bounds.size.height)
+      drawn_dashes = drawn_dashes + 1
+    end
+    CATransaction.commit
+  end
+
 
   def create_arrow_view
     # First time initialization.
